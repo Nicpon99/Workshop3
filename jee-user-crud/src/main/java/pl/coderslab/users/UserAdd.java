756dc2005238c6e7c5189f55d72a30a7ba1c1173
Nicpon99 <Nicpon044@onet.pl>
2023-03-21 19:09:59 +0100

    początek user add

---
+++ jee-user-crud/src/main/java/pl/coderslab/users/UserAdd.java
@@ -0,0 +1,29 @@
+package pl.coderslab.users;
+
+import pl.coderslab.utils.DbUtil;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/user/add")
+public class UserAdd extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (Connection connection = DbUtil.getConnection()){
+
+
+            getServletContext().getRequestDispatcher("/users/add.jsp").forward(request, response);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
